cf27fe838a1875196e2cdba1b3d98d36876f8bde
chaz8081 <charles.scholle1+bread@gmail.com>
2024-01-07 14:49:17 -0500

    update firefox setup

--- scripts/firefox/set_css.sh
+++ scripts/firefox/set_css.sh
@@ -7,15 +7,15 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # add the css to all profiles
 for d in $HOME/Library/Application\ Support/Firefox/Profiles/*/ ; do
-    echo "adding chrome directory to $d"
+    echo "creating chrome directory if it doesn't exist: $d"
     mkdir -p chrome
    
-    echo "copying in userChrome.css"
+    echo "copying userChrome.css"
     cp $SCRIPT_DIR/userChrome.css $d/chrome
 done
 
-echo "Firefox CSS is g2g"
-echo "Don't forget to visit about:config and set"
+echo "firefox css is g2g"
+echo "don't forget to visit about:config and set"
 echo "toolkit.legacyUserProfileCustomizations.stylesheets = true"
 
 exit 0
