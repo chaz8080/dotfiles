b64307c8f12e3d06fcd2edde0d7e90d2347405a0
chaz8080 <charles.scholle1@gmail.com>
2025-02-16 20:23:37 -0500

    add mlx-lm and start building defaults

---
+++ scripts/macos/defaults.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# link: https://github.com/pawelgrzybek/dotfiles/blob/master/setup-macos.sh
+# TODO: test this script to run...
+
+# Menu Bar Only > Spotlight > Don't Show in Menu Bar
+defaults -currentHost write com.apple.Spotlight MenuItemHidden -int 1
+
+# Don't Ask Siri
+defaults write com.apple.Siri SiriPrefStashedStatusMenuVisible -bool false
+defaults write com.apple.Siri VoiceTriggerUserEnabled -bool false
+
+# Dock > Size:
+defaults write com.apple.dock tilesize -int 36
+
+# Dock > Magnification
+defaults write com.apple.dock largesize -int 54
+
+# Dock > Magnification
+defaults write com.apple.dock magnification -bool true
+
+# Dock > Minimize windows using: Scale effect
+defaults write com.apple.dock mineffect -string "scale"
+
+# Dock > Minimize windows into application icon
+defaults write com.apple.dock minimize-to-application -bool true
+
+# Dock > Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+
+# Dock > Automatically hide and show the Dock (duration)
+defaults write com.apple.dock autohide-time-modifier -float 0.4
+
+# Dock > Automatically hide and show the Dock (delay)
+defaults write com.apple.dock autohide-delay -float 0
+
+# Show recent applications in Dock
+defaults write com.apple.dock "show-recents" -bool false
+
+# Windows & Apps > Prefer tabs when opening documents
+defaults write -globalDomain AppleWindowTabbingMode -string "always"
+
+# Desktop & Stage Manager > Click Wallpaper to reveal desktop > Only in Stage Manager
+defaults write "com.apple.WindowManager" EnableStandardClickToShowDesktop -bool false
+
+# Mission Controll > Automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool false
+
+# Txt Input > Capitalise words automatically
+defaults write -globalDomain NSAutomaticCapitalizationEnabled -bool false
+
+# Txt Input > Add full stop with double-space
+defaults write -globalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+
+
+# Show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Show warning before changing an extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Show warning before removing from iCloud Drive
+defaults write com.apple.finder FXEnableRemoveFromICloudDriveWarning -bool false
+
+# Finder > View > As List
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+
+# Finder > View > Show Path Bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Kill affected apps
+for app in "Dock" "Finder"; do
+  killall "${app}" > /dev/null 2>&1
+done
